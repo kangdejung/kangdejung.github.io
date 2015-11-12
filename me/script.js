45e4337b29db3f1c85fa4d07cde8f52f400f1f60
webplug <webplug@gmail.com>
2015-11-12 09:23:22 +0900

    10

---
+++ me/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$("#btn-yes").click(function(){
+		$("#result").html("<img src='yes.jpg' style='width:300px;'>");
+	});
+
+	$("#btn-no").click(function(){
+		$("#result").html("<img src='no.jpg' style='width:300px;'>");
+	});	
+});
